7d5002959298e40dab295e5e3a93465b8e7fec33
Jitesh <jchavan010@gmail.com>
2025-05-03 21:09:34 -0400

    dethwise convolutional positional embedding with adaptiveLayernorm modulation with conditioning C

--- mamba_diffusion/model/pe/cpe.py
+++ mamba_diffusion/model/pe/cpe.py
@@ -3,8 +3,6 @@
 import torch.nn as nn
 
 
-def modulate(x, shift, scale):
-    return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
 
 
 class PosCNN(nn.Module):
@@ -25,6 +23,7 @@ class PosCNN(nn.Module):
     def forward(self, x, H, W):
         # T = H * W
         B, T, C = x.shape
+        assert H * W == T, f"spatial resolution incoherent, H:{H}*W:{W} must be T:{T}"
         x = x.transpose(1, 2).view(B, C, H, W) # (B, C, H, W)
         if self.stride == 1:
             # the fact that we are adding residual without channel up implies that they have the same channels
@@ -48,24 +47,41 @@ class PosCNN(nn.Module):
 
 class AdaInPosCNN(nn.Module):
     def __init__(self, in_channels, n_embd=768, stride=1):
-        super(AdaInPosCNN, self).__init__()
+        super().__init__()
         self.proj = nn.Sequential(nn.Conv2d(in_channels, n_embd, 3, stride, padding=1, bias=True, groups=n_embd))
         self.adaLN_modulation = nn.Sequential(nn.SiLU(), nn.Linear(n_embd, 2 * n_embd, bias=True))
         self.norm = nn.LayerNorm(n_embd)
         self.stride = stride
 
     def forward(self, x, c, H, W):
-        B, N, C = x.shape
-        feat_token = x
-        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
-        cnn_feat = feat_token.transpose(1, 2).view(B, C, H, W)
-        if self.s == 1:
-            x = self.proj(cnn_feat) + cnn_feat
+        B, T, C = x.shape # B, T, C
+        assert H * W == T, f"spatial resolution incoherent, H:{H}*W:{W} must be T:{T}"
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1) # (B, C), (B, C)
+        
+        x = x.transpose(1, 2).view(B, C, H, W)
+        if self.stride == 1:
+            # residual addition without channel_up only works if shapes are the same implies that in_channels = n_embd and that it's depthwise convolution
+            x = self.proj(x) + x # (B, C, H, W)
         else:
-            x = self.proj(cnn_feat)
-        x = x.flatten(2).transpose(1, 2)
+            x = self.proj(x) # (B, C', H, W) wont scale in deep networks due to lack of residuals
+        # revert to original shape
+        x = x.flatten(2).transpose(1, 2) #(B, T, C)
         x = modulate(self.norm(x), shift, scale)
         return x
 
     def no_weight_decay(self):
         return ["proj.%d.weight" % i for i in range(4)]
+
+
+def modulate (x, scale, shift):
+    # scale and shift are derived from conditioning C, that is usually pooled across T
+    # in case of text, captions are B,T,C but noise is just B,C
+    # B,T,C is pooled across T to get B,C
+    # scale shift are derived from conditioning C, nonlinearity -> C to 2C
+
+    # in atypical setting if contribution of initialized weights maybe 0 to avoid propagation of zeros
+    # we add 1 to scale so its basically identity transformation on X
+    # in atypical setting
+
+    # broadcasting takes care of the rest
+    return x * (1+ scale.unsqueeze(1)) + shift.unsqueeze(1) # (B, T, C) * (B, 1, C) + (B, 1, C)
